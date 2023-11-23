1fdca853f7f9d31fd81b3b3f4efe770e92fc451b
atarutin <andrey.tarutin@lykke-business.ch>
2023-11-23 17:46:23 +0300

    Revert "fix(LT-4412): update RabbitMQ.Client anf core microsoft libraries"
    This reverts commit aaae07361a9bc3cfe712f05dd593d685bc5262f4.

---
+++ src/Lykke.SettingsReader/SettingsProcessor/Checkers/MongoChecker.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using MongoDB.Driver;
+using MongoDB.Driver.Core.Clusters;
+
+namespace Lykke.SettingsReader.Checkers
+{
+    internal class MongoChecker : ISettingsFieldChecker
+    {
+        public CheckFieldResult CheckField(object model, string propertyName, string value)
+        {
+            try
+            {
+                var mongoUrl = MongoUrl.Create(value);
+
+                var client = new MongoClient(mongoUrl);
+                
+                client.ListDatabases();
+                
+                return client.Cluster.Description.State == ClusterState.Connected
+                    ? CheckFieldResult.Ok(propertyName, value)
+                    : CheckFieldResult.Failed(propertyName, value);
+            }
+            catch (MongoConfigurationException)
+            {
+                return CheckFieldResult.Failed(propertyName, value);
+            }
+            catch (TimeoutException)
+            {
+                return CheckFieldResult.Failed(propertyName, value);
+            }
+        }
+    }
+}
